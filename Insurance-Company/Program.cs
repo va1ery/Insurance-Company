071e245d102103d901018f3b594ae96a10976f07
ssmlnsk <ssmlnsk@mail.ru>
2020-10-21 23:15:01 +0300

    Добавьте файлы проекта.

---
+++ Insurance-Company/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Insurance_Company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
